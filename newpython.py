03ad0e29e6733feba5642feab81dbd7c7fe18b9a
AndrewWhiteZ <ksarion1@gmail.com>
2022-11-28 16:26:29 +0500

    Change enter log

--- newpython.py
+++ newpython.py
@@ -1,7 +1,7 @@
 import statsWriter
 import tableWriter
 
-output_type = input('Выберите способ представления выходных данных (Вакансии/Статистика): ')
+output_type = input('Выберите возможность представления выходных данных (Вакансии/Статистика): ')
 if output_type == 'Вакансии':
     tableWriter.PrintTable()
 elif output_type == 'Статистика':
